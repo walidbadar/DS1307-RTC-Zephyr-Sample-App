5569def9adebc73813e9e214956d05d8491c3b21
Muhammad Waleed Badar <81442784+walidbadar@users.noreply.github.com>
2024-12-09 00:42:33 +0500

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,28 +1,29 @@
 /*
- * Copyright (c) 2024, Muhammad Waleed Badar
+ * Copyright (c) 2024 Muhammad Waleed Badar
  *
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#include <stdio.h>
+#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/rtc.h>
-#include <zephyr/kernel.h>
 #include <zephyr/sys/util.h>
 
-#define RTC_NODE DT_NODELABEL(rtc0)
+const struct device *const rtc = DEVICE_DT_GET(DT_NODELABEL(rtc0));
 
-static int set_date_time(const struct device *dev)
+static int set_date_time(const struct device *rtc)
 {
 	int ret = 0;
-	struct rtc_time tm = {.tm_year = 2024 - 1900,
-			      .tm_mon = 7 - 1,
-			      .tm_mday = 28,
-			      .tm_hour = 3,
-			      .tm_min = 51,
-			      .tm_sec = 0};
+	struct rtc_time tm = {
+		.tm_year = 2024 - 1900,
+		.tm_mon = 11 - 1,
+		.tm_mday = 17,
+		.tm_hour = 4,
+		.tm_min = 19,
+		.tm_sec = 0,
+	};
 
-	ret = rtc_set_time(dev, &tm);
+	ret = rtc_set_time(rtc, &tm);
 	if (ret < 0) {
 		printk("Cannot write date time: %d\n", ret);
 		return ret;
@@ -30,18 +31,18 @@ static int set_date_time(const struct device *dev)
 	return ret;
 }
 
-static int get_date_time(const struct device *dev)
+static int get_date_time(const struct device *rtc)
 {
 	int ret = 0;
 	struct rtc_time tm;
 
-	ret = rtc_get_time(dev, &tm);
+	ret = rtc_get_time(rtc, &tm);
 	if (ret < 0) {
 		printk("Cannot read date time: %d\n", ret);
 		return ret;
 	}
 
-	printk("Current RTC time: %04d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900,
+	printk("RTC date and time: %04d-%02d-%02d %02d:%02d:%02d\n", tm.tm_year + 1900,
 	       tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 
 	return ret;
@@ -49,27 +50,17 @@ static int get_date_time(const struct device *dev)
 
 int main(void)
 {
-	int ret = 0;
-
-	/* Get the device structure for the RTC */
-	const struct device *const dev = DEVICE_DT_GET(RTC_NODE);
-
-	/* Check if the RTC is available */
-	if (dev == NULL) {
-		printk("Error: no device found\n");
+	/* Check if the RTC is ready */
+	if (!device_is_ready(rtc)) {
+		printk("Device is not ready\n");
 		return 0;
 	}
 
-	set_date_time(dev);
+	set_date_time(rtc);
 
-	/* Continuously read the current time from the RTC */
-	while (1) {
-		ret = get_date_time(dev);
-		if (ret < 0) {
-			printk("Failed to read from rtc\n");
-			return 0;
-		}
+	/* Continuously read the current date and time from the RTC */
+	while (get_date_time(rtc) == 0) {
 		k_sleep(K_MSEC(1000));
-	}
+	};
 	return 0;
 }
